726a0e2ad8fc5374591ed3f4a518d4dc8cb05fe7
Ajmalk2002 <ajmal.2001011@srec.ac.in>
2026-02-13 14:51:46 +0530

    13/02/26

---
+++ fragment-layout_demo-2/src/main/java/com/example/demo/FragmentLayoutDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentLayoutDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmentLayoutDemo2Application.class, args);
+	}
+
+}
---
+++ fragment-layout_dmeo/src/main/java/com/example/demo/FragmentLayoutDmeoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentLayoutDmeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmentLayoutDmeoApplication.class, args);
+	}
+
+}
